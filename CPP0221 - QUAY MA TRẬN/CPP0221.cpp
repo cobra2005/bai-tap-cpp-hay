71ddbe5c8f7cd531925d62f0c5eb24a6a961c66e
zeros <thaicobra1610@gmail.com>
2025-11-22 21:32:40 +0700

    fix(CPP0221): Rewrite the implementation of the spiral matrix algorithm.

--- CPP0221 - QUAY MA TRẬN/CPP0221.cpp
+++ CPP0221 - QUAY MA TRẬN/CPP0221.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// Nên làm bài CPP0223 - MA TRẬN XOẮN ỐC - 1 trước trước khi làm bài tập này
+
 int main() {
     int t; cin >> t;
     while(t--) {
@@ -11,30 +13,36 @@ int main() {
     			cin >> a[i][j];
 			}
 		}
-		int layers = (m < n ? m : n) / 2;
-		for(int layer = 0; layer < layers; layer++) {
+		int top = 0, bottom = n - 1;
+		int left = 0, right = m - 1;		
+		while(top <= bottom && left <= right) {
+			int top1 = top, bottom1 = bottom, left1 = left, right1 = right;
 			vector<int> v;
-			int top = layer;
-			int left = layer;
-			int bottom = n - 1 - layer;
-			int right = m - 1 - layer;
 			for(int i = left; i <= right; i++) v.push_back(a[top][i]);
-			for(int i = top + 1; i <= bottom; i++) v.push_back(a[i][right]);
-			if(bottom > top) {
-				for(int i = right - 1; i >= left; i--) v.push_back(a[bottom][i]);
+			top++;
+			for(int i = top; i <= bottom; i++) v.push_back(a[i][right]);
+			right--;
+			if(top <= bottom) {
+				for(int i = right; i >= left; i--) v.push_back(a[bottom][i]);
+				bottom--;
 			}
-			if(left < right) {
-				for(int i = bottom - 1; i > top; i--) v.push_back(a[i][left]);
+			if(left <= right) {
+				for(int i = bottom; i >= top; i--) v.push_back(a[i][left]);
+				left++;
 			}
 			rotate(v.rbegin(), v.rbegin() + 1, v.rend());
 			int idx = 0;
-			for(int i = left; i <= right; i++) a[top][i] = v[idx++];
-			for(int i = top + 1; i <= bottom; i++) a[i][right] = v[idx++];
-			if(bottom > top) {
-				for(int i = right - 1; i >= left; i--) a[bottom][i] = v[idx++];
+			for(int i = left1; i <= right1; i++) a[top1][i] = v[idx++];
+			top1++;
+			for(int i = top1; i <= bottom1; i++) a[i][right1] = v[idx++];
+			right1--;
+			if(top1 <= bottom1) {
+				for(int i = right1; i >= left1; i--) a[bottom1][i] = v[idx++];
+				bottom1--;
 			}
-			if(left < right) {
-				for(int i = bottom - 1; i > top; i--) a[i][left] = v[idx++];
+			if(left1 <= right1) {
+				for(int i = bottom1; i >= top1; i--) a[i][left1] = v[idx++];
+				left1++;
 			}
 		}
 		for(int i = 0; i < n; i++) {
