33a0ced3c783c5a5144135c53376ef201e20a222
zeros <thaicobra1610@gmail.com>
2025-11-22 21:34:04 +0700

    CPP0223 - MA TRẬN XOẮN ỐC - 1

---
+++ CPP0223 - MA TRẬN XOẮN ỐC - 1/CPP0223.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t; cin >> t;
+    while(t--) {
+    	int n, m; cin >> n >> m;
+    	int a[n][m];
+    	for(int i = 0; i < n; i++) {
+    		for(int j = 0; j < m; j++) {
+    			cin >> a[i][j];
+			}
+		}
+		int top = 0, bottom = n - 1;
+		int left = 0, right = m - 1;
+		while(top <= bottom && left <= right) {
+			for(int i = left; i <= right; i++) cout << a[top][i] << " ";
+			top++;
+			for(int i = top; i <= bottom; i++) cout << a[i][right] << " ";
+			right--;
+			if(top <= bottom) {
+				for(int i = right; i >= left; i--) cout << a[bottom][i] << " ";
+				bottom--;	
+			}
+			if(left <= right) {
+				for(int i = bottom; i >= top; i--) cout << a[i][left] << " ";
+				left++;	
+			}
+		}
+		cout << endl;
+	}
+    return 0;
+}
